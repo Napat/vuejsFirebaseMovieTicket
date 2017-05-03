060c0f21e8de1e5b55ed35f8e0bface685dba132
Napat Rungruangbangchan <napat.joe@gmail.com>
2017-05-03 23:57:01 +0700

    add Computed Properties for complex logic to get  seat info then show in template. - about Computed Prop: https://vuejs.org/v2/guide/computed.html

--- vue.js-movie/src/components/Seat.vue
+++ vue.js-movie/src/components/Seat.vue
@@ -1,11 +1,21 @@
 <template>
     <div class="box">
         <h3 class="title">[[Seat]] {{ movieId }}</h3>
+        <p> {{ this.seats }} </p>
     </div>
 </template>
 
 <script>
+import movie from 'Others/movie.json'
+console.log('xxxx: ')
+console.log(movie)
+
 export default {
     props: [ 'movieId'],
+    computed: {
+        seats() {
+            return movie[this.movieId]
+        }
+    }
 }
 </script>
