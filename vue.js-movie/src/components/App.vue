0205dd2ea85854dd02133a1f7037d5c404059743
Napat Rungruangbangchan <napat.joe@gmail.com>
2017-05-08 23:43:33 +0700

    implement google sign in/out

--- vue.js-movie/src/components/App.vue
+++ vue.js-movie/src/components/App.vue
@@ -1,6 +1,14 @@
 <template>
     <div class="box">
     <h3 class="title">[App]: {{ movieId }}</h3>
+    <div id="googlemail">
+        <button @click="googlesign()"> signin </button>
+        <button @click="googlesignOut()"> signout </button>
+        <br>
+        <img :src="photoURL" height="100" width="100" alt="LoginImg" > 
+        <br>
+        {{ displayName }}
+    </div>    
     <p> Select: {{ status.count }} seat(s), Total price: {{ status.price }} Baht</p>
     <movie @chooseMovie="handleChooseMovie" :movieId="movieId" />
     <seat 
@@ -9,7 +17,7 @@
         :selectSeats="selectSeats" 
         :firebaseSeats="firebaseSeats" 
     />
-    </div>
+  </div>
 </template>
 
 <script>
@@ -22,16 +30,17 @@ import { pushToArray } from 'Others/lib'
 import { firebaseconfig } from 'Others/config'
 
 firebase.initializeApp(firebaseconfig);
-var provider = new firebase.auth.FacebookAuthProvider()
+var provider = new firebase.auth.GoogleAuthProvider()
 
-provider.addScope('public_profile')
+provider.addScope('https://www.googleapis.com/auth/plus.login');
 provider.setCustomParameters({
-  'display': 'popup'
+  'login_hint': 'user@example.com'
 })
 
 const db = firebase.database()
 
 export default {
+    name: 'app',
     components: { Movie, Seat },
     data() {
         return {
@@ -40,7 +49,9 @@ export default {
             movieId: '',
             selectSeats: [],
             firebaseSeats: [],
-            status: { count: 0, price: 0 }
+            status: { count: 0, price: 0 },
+            displayName: '',
+            photoURL: ''
         }
     },
     methods: {
@@ -83,8 +94,49 @@ export default {
                 return sum
                 }, { count: 0, price: 0 } 
             )
+        },
+        googlesign () {
+            console.log('yes')
+            var vm = this
+            firebase.auth().signInWithPopup(provider).then(function (result) {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                var token = result.credential.accessToken
+                // The signed-in user info.
+                var user = result.user
+                console.log(token, user)
+                console.log('displayName :: ', user.displayName)
+                console.log('photoURL ::', user.photoURL)
+                vm.displayName = user.displayName
+                vm.photoURL = user.photoURL
+            }).catch(function (error) {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                var email = error.email;
+                var credential = error.credential;
+                console.log(error)
+            })
+        },
+        googlesignOut () {
+            var vm = this
+            firebase.auth().signOut().then(function () {
+                console.log('logOut')
+                vm.displayName = ''
+                vm.photoURL = ''
+            }, function (error) {
+                console.log(error)
+            })
         }
     }
 }
-
 </script>
+
+<style>
+#googlemail {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
